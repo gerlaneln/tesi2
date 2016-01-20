a998b5d0a71883e746940a2c1f64e8f315024e90
gerlaneln <g2fjac@gmail.com>
2016-01-20 11:03:15 -0500

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,10 @@
 public Class Calculadora{
   public static void main(String [] args){
-  int a,b;
-  System.out.println(a+b);
+    Scanner ler = new Scanner.System.in();
+    int a, b, opcoes;
+    System.out.println("Entre com os valores de a e b: ");
+    a = ler.nextInt();
+    b = ler.nextInt();
+    System.out.println(a+" + "+b+" = "+(a+b));
   }
 }  
