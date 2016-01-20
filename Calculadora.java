e4516c1e38cd8f2116eec4b77fa531ce0f368d49
gerlaneln <g2fjac@gmail.com>
2016-01-20 10:54:29 -0500

    Update Calculadora.java

--- Calculadora.java
+++ Calculadora.java
@@ -1,6 +1,24 @@
 public Class Calculadora{
   public static void main(String [] args){
-  int a,b;
-  System.out.println(a+b);
+    Scanner ler = new Scanner.System.in;
+    int a, b, opcao;
+    System.out.println("Entre com os valores de a e b: ");
+    a = ler.nextInt();
+    b = ler.nextInt();
+    System.out.println("Selecione a operação: ");
+    System.out.println("1 Soma");
+    System.out.println("2 Subtração");
+    System.out.println("3 Divisão");
+    System.out.println("4 Multiplicação");
+    opcao = ler.nextInt();
+    case 1:
+      System.out.println(a+b);
+    case 2:
+      System.out.println(a-b);
+    case 3:
+      System.out.println(a/b);
+    case 4:
+      System.out.println(a*b);
+  
   }
 }  
